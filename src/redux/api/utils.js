3eee34610d92e4e78d50ec0d448f4dbe3b8719f4
Ahsanali059 <AhsanDev.org@gmail.com>
2024-05-25 17:52:48 +0500

    added authInterceptor

---
+++ src/redux/api/utils.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import process from "../../../.eslintrc.cjs";
+
+const BASE_URL = "http://localhost:3000"
+
+const ADMIN_URL = `${REACT_APP_API_URL}/admin`;
+
+/**
+ *
+ * @param request
+ * @returns {*}
+ */
+const authInterceptor = (request)=>{
+    const accessToken = JSON.parse(localStorage.getItem("profile"))?.accessToken;
+    if (accessToken) {
+        request.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return request;
+}
+
+/**
+ *
+ * @param req
+ * @returns {*}
+ */
+const adminAuthInterceptor = (req) => {
+    const accessToken = JSON.parse(localStorage.getItem("admin"))?.accessToken;
+    if (accessToken) {
+        req.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return req;
+};
+
+
+
+
