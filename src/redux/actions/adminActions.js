4e0fc44f5ddd31eb1e3f0a5927b62a0e015e510f
Ahsanali059 <AhsanDev.org@gmail.com>
2024-06-07 23:47:29 +0500

    added get cummunities actions

--- src/redux/actions/adminActions.js
+++ src/redux/actions/adminActions.js
@@ -98,3 +98,111 @@ export const getServicePreferencesAction=()=> async (dispatch) => {
     }
 };
 
+
+export const updateServicePreferencesAction =
+    (preferences) => async (dispatch) => {
+        try {
+            const { error } = await api.updateServicePreferences(preferences);
+            if (error) {
+                throw new Error(error);
+            }
+            dispatch({
+                type: types.UPDATE_SERVICE_PREFERENCES_SUCCESS,
+            });
+        } catch (error) {
+            dispatch({
+                type: types.UPDATE_SERVICE_PREFERENCES_FAIL,
+                payload: error.message,
+            });
+        }
+    };
+
+export const getCommunitiesAction = () => async (dispatch) => {
+    try {
+        const { error, data } = await api.getCommunities();
+        if (error) {
+            throw new Error(error);
+        }
+        dispatch({
+            type: types.GET_COMMUNITIES_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: types.GET_COMMUNITIES_FAIL,
+            payload: error.message,
+        });
+    }
+};
+export const getCommunityAction = (communityId) => async (dispatch) => {
+    try {
+        const { error, data } = await api.getCommunity(communityId);
+        if (error) {
+            throw new Error(error);
+        }
+
+        dispatch({
+            type: types.GET_COMMUNITY_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: types.GET_COMMUNITY_FAIL,
+            payload: error.message,
+        });
+    }
+};
+
+export const getModeratorsAction = () => async (dispatch) => {
+    try {
+        const { error, data } = await api.getModerators();
+        if (error) {
+            throw new Error(error);
+        }
+        dispatch({
+            type: types.GET_MODERATORS_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: types.GET_MODERATORS_FAIL,
+            payload: error.message,
+        });
+    }
+};
+
+export const addModeratorAction =
+    (communityId, moderatorId) => async (dispatch) => {
+        try {
+            const { error } = await api.addModerator(communityId, moderatorId);
+            if (error) {
+                throw new Error(error);
+            }
+            dispatch({
+                type: types.ADD_MODERATOR_SUCCESS,
+            });
+        } catch (error) {
+            dispatch({
+                type: types.ADD_MODERATOR_FAIL,
+                payload: error.message,
+            });
+        }
+    };
+
+export const removeModeratorAction =
+    (communityId, moderatorId) => async (dispatch) => {
+        try {
+            const { error } = await api.removeModerator(communityId, moderatorId);
+            if (error) {
+                throw new Error(error);
+            }
+            dispatch({
+                type: types.REMOVE_MODERATOR_SUCCESS,
+            });
+        } catch (error) {
+            dispatch({
+                type: types.REMOVE_MODERATOR_FAIL,
+                payload: error.message,
+            });
+        }
+    };
